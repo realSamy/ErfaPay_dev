bdbaeca5db0c287b4ebbf8c13cbdcb1c73a8b68d
realSamy <imhoodaji@gmail.com>
2025-12-31 02:33:11 +0330

    update

--- backend/config/settings/prod.py
+++ backend/config/settings/prod.py
@@ -1,6 +1,8 @@
 from base import *
 from celery.schedules import crontab
 
+DEBUG = os.environ.get('DEBUG', False) in ['True', 'true', True]
+
 CELERY_BROKER_URL = os.environ.get('CELERY_BROKER_URL', 'redis://redis:6379/0')
 CELERY_RESULT_BACKEND = os.environ.get('CELERY_RESULT_BACKEND', 'redis://redis:6379/0')
 CELERY_BEAT_SCHEDULE = {
@@ -17,3 +19,8 @@ EMAIL_USE_TLS = True
 EMAIL_HOST_USER = os.environ.get('SMTP_USER')
 EMAIL_HOST_PASSWORD = os.environ.get('SMTP_PASSWORD')
 DEFAULT_FROM_EMAIL = os.environ.get('SMTP_FROM')
+
+DOMAIN = os.environ.get('DOMAIN')
+if DOMAIN:
+    CORS_ALLOWED_ORIGINS = [f'https://{DOMAIN}']
+    ALLOWED_HOSTS = [f'https://{DOMAIN}']
