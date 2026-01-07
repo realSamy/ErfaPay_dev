135ac57271b5c2f37751f203f193d35a8bd3e0da
realSamy <imhoodaji@gmail.com>
2026-01-07 06:33:53 +0330

    update

--- frontend/app/composables/useLoadGlobalSettingsStore.ts
+++ frontend/app/composables/useLoadGlobalSettingsStore.ts
@@ -1,7 +1,7 @@
 import type {GlobalSettings} from "~/types/settings";
 
 export const useLoadGlobalSettingsStore = async () => {
-  const store = useState<GlobalSettings>('global.settings', () => ({} as GlobalSettings))
+  const store = useState<GlobalSettings|undefined>('global.settings', () => ({} as GlobalSettings))
   if (!store.value || Object.keys(store.value).length === 0) {
     const {fetchSettings} = useGlobalSettings()
     store.value = await fetchSettings()
